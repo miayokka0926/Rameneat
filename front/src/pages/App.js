688cbb5b02071510ef9d196c1788186e0e8d4b06
huagesun <80430464+huagesun@users.noreply.github.com>
2021-05-07 01:40:54 +0800

    login UI update

--- front/src/pages/App.js
+++ front/src/pages/App.js
@@ -1,59 +1,62 @@
-import {useState, useEffect} from 'react';
-import {Jumbotron, Button, OverlayTrigger, Tooltip, Modal, Form, FormControl} from 'react-bootstrap';
-import {message, Typography} from 'antd';
+import { useState, useEffect } from 'react';
+import { Jumbotron, Button, OverlayTrigger, Tooltip, Form, FormControl } from 'react-bootstrap';
+import Collapse from 'react-bootstrap/Collapse';
+import { message, Typography } from 'antd';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'antd/dist/antd.css';
 import axios from '../commons/axios.js';
-import image from '../logo.png';
-const {Link} = Typography;
+import image from '../pic/logo1.jpg';
+const { Link } = Typography;
 
 
 function App(props) {
 
-  const [show, setShow] = useState(false);
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-  const[email, setEmail] = useState('');
-  const[password,setPassword] = useState('');
+  // const [show, setShow] = useState(false);
+  // const handleClose = () => setShow(false);
+  // const handleShow = () => setShow(true);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   const [lat, setLat] = useState('');
   const [lng, setLng] = useState('');
   const [vendors, setVendors] = useState([]);
 
+  const [open, setOpen] = useState(false);
+
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(function(position) {
+    navigator.geolocation.getCurrentPosition(function (position) {
       setLat(position.coords.latitude)
       setLng(position.coords.longitude)
     })
-    axios.get('/vendor?lat='+lat+'&lng='+lng).then(response => {
+    axios.get('/vendor?lat=' + lat + '&lng=' + lng).then(response => {
       console.log(response)
       setVendors(response.data.vendors)
     })
-  },[lat,lng])
+  }, [lat, lng])
 
 
   const renderTooltip = (props) => (
     <Tooltip id="button-tooltip" {...props}> feature opening soon </Tooltip>
   )
 
-  const renderTooltipCustomer = (props) => (
-        <Tooltip id="button-tooltip" {...props}> start your tasty journey</Tooltip>
+  const renderTooltipCustomer = (props) => (
+    <Tooltip id="button-tooltip" {...props}> start your tasty journey</Tooltip>
   )
 
   const onLogin = () => {
-    axios.post('/customer/login', {email: email, password:password}).then(response=>{
-      if (response.data.success){
+    axios.post('/customer/login', { email: email, password: password }).then(response => {
+      if (response.data.success) {
         props.history.push('/customer', {
           customer: response.data.customer,
           vendors: vendors,
           position: [lat, lng]
         })
-      }else {
+      } else {
         message.error(response.data.error)
-        setShow(false);
+        // setShow(false);
       }
-    }).catch(error =>{
-      
+    }).catch(error => {
+
       console.log(error.response.data.message)
       message.error(error.response.data.message)
     })
@@ -71,56 +74,89 @@ function App(props) {
   }
 
   return (
-    <div style={{width: '60%', margin: 'auto',marginTop:'7%'}}>
-      <Modal show = {show} onHide={handleClose} style={{marginTop:'2vh'}}>
-        <Modal.Header closeButton>
-          <Modal.Title style={{color:'orange'}}>Hi, please log in</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="email" placeholder="Please enter your email"
-              onChange={e => setEmail(e.target.value)} />
 
-            </Form.Group>
-            <Form.Group controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <FormControl type="password" placeholder="Password"
-              onChange = {e=>setPassword(e.target.value)} />
-            </Form.Group>
-          </Form>
-          <OverlayTrigger placement="right" delay={{show:250, hide:400}} overlay={renderTooltip}>
-          <Link onClick={findPassword}>Forget Password?</Link>
-          </OverlayTrigger>
-          <p></p>
-          <Link onClick={onSkip}>Skip</Link>
-        </Modal.Body>
-        <Modal.Footer>
-        
-          <Button variant="secondary" size="lg" block style={{width: '25vw',color: '#F4976C', backgroundColor: '#FBE8A6', borderColor: '#FBE8A6'}}>Cancel</Button>
-          <Button variant="primary" onClick={onLogin} size="lg" block style={{width: '25vw',color: '#FBE8A6', backgroundColor: '#F4976C', borderColor: '#F4976C'}}>Login</Button>
-        </Modal.Footer>
-        </Modal>
-        <Jumbotron style={{width: '30%', backgroundColor: 'white', margin: 'auto', marginLeft:"15%", marginTop:'1%'}}>
+    <div id="LogIn" style={{ width: '60%', margin: 'auto', marginTop: '2%' }}>
+      <Jumbotron style={{ width: '50%', backgroundColor: 'white', margin: 'auto' }}>
+
         <center>
-        <img src={image} style={{width: '35vw', margin: 'auto'}} alt="logo"/>
-        </center >
-        <br /><br />
-          <h1 style={{color:"orange"}}>Welcome to Ramen Eat!</h1>
-          <br />
-          <h6 style={{color: '#707070', opacity: '80%'}}>I'm a ...</h6>
-          <p>
-          
-          <OverlayTrigger placement="right" delay={{show:250, hide:400}} overlay={renderTooltipCustomer}>
-          <Button variant="primary" onClick={handleShow} size="lg" block style={{width: '37vw', color: '#FBE8A6', backgroundColor: '#F4976C', borderColor: '#F4976C'}}>Customer</Button>
+          <img src={image} style={{ width: '100%', margin: 'auto' }} alt="logo" />
+        </center>
+
+        <br />
+
+        <h1 style={{ color: "#F4976C" }}>Welcome!</h1>
+        <h6 style={{ color: '#707070' }}>Please choose your identity below</h6>
+
+        <br />
+
+        <p>
+          <h6 style={{ color: '#707070', opacity: '50%' }}>I'm a...</h6>
+        </p>
+
+        <p>
+          <OverlayTrigger placement="right" delay={{ show: 250, hide: 400 }} overlay={renderTooltipCustomer}>
+            <Button
+              onClick={() => setOpen(!open)}
+              aria-controls="example-collapse-text"
+              aria-expanded={open}
+              variant="primary"
+              size="lg"
+              block
+              style={{ color: '#FBE8A6', backgroundColor: '#F4976C', borderColor: '#F4976C' }}
+            >
+              Customer
+          </Button>
           </OverlayTrigger>
-          <br />
-          <OverlayTrigger placement="right" delay={{show:250, hide:400}} overlay={renderTooltip}>
-          <Button variant="primary" size="lg" block style={{width: '37vw',color: '#F4976C', backgroundColor: '#FBE8A6', borderColor: '#FBE8A6'}}>Vendor</Button>
+
+          <Collapse in={open}>
+            <p>
+              <Form>
+                <br />
+                <Form.Group controlId="formBasicEmail">
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control type="email" placeholder="Please enter your email"
+                    onChange={e => setEmail(e.target.value)} />
+                  <Form.Text className="text-muted">
+                    Your info is secured with us.
+          </Form.Text>
+                </Form.Group>
+                <Form.Group controlId="formBasicPassword">
+                  <Form.Label>Password</Form.Label>
+                  <FormControl type="password" placeholder="Password"
+                    onChange={e => setPassword(e.target.value)} />
+                </Form.Group>
+              </Form>
+
+              <p> <Link onClick={findPassword}>Forget Password?</Link> </p>
+              <p> <Link onClick={onSkip}>Skip</Link> </p>
+
+              <Button
+                variant="primary"
+                onClick={onLogin}
+                size="lg"
+                block
+                style={{ color: '#F4976C', backgroundColor: '#FBE8A6', borderColor: '#FBE8A6' }}>
+                Login
+              </Button>
+            </p>
+          </Collapse>
+        </p>
+
+
+        <div>
+          <OverlayTrigger placement="right" delay={{ show: 250, hide: 400 }} overlay={renderTooltip}>
+            <Button
+              variant="primary"
+              size="lg"
+              block
+              style={{ color: '#707070', backgroundColor: '#F3F3F3', borderColor: '#F3F3F3' }}>
+              Vendor
+            </Button>
           </OverlayTrigger>
-          </p>
-        </Jumbotron>
+        </div>
+
+
+      </Jumbotron>
     </div>
   );
 }
