d9b6bd91d4e52efda79b80bd104ee98669517a70
huagesun <huages@student.unimelb.edu.au>
2021-05-30 17:08:38 +0800

    heroku

--- front/src/commons/axios.js
+++ front/src/commons/axios.js
@@ -1,11 +1,11 @@
 import _axios from 'axios';
 // base url
-const axios = baseUrl => {
+const axios = (baseUrl) => {
     const instance = _axios.create({
         baseURL:"https://pacific-spire-87195.herokuapp.com/" || 'http://localhost:3000'
     })
     return instance;
 }
 
-export{ axios};
+export{ axios };
 export default axios();
--- src/app.js
+++ src/app.js
@@ -89,7 +89,6 @@ app.use('/order', order);
 
 //serve static assets
 if (process.env.NODE_ENV === 'production') {
-
     app.use(express.static('client/build'));
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
