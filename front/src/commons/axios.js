f8edc632b098915ad8a5d5ab8c3d1bb713d6e47a
huagesun <huages@student.unimelb.edu.au>
2021-05-30 15:56:03 +0800

    heroku

--- front/src/commons/axios.js
+++ front/src/commons/axios.js
@@ -2,7 +2,7 @@ import _axios from 'axios';
 // base url
 const axios = baseUrl => {
     const instance = _axios.create({
-        baseURL:baseUrl || 'http://localhost:3000'
+        baseURL:"https://pacific-spire-87195.herokuapp.com" || 'http://localhost:3000'
     })
     return instance;
 }
