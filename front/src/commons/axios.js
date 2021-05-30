e057a099d888a45b76c6d1272dbf5e097930277c
huagesun <huages@student.unimelb.edu.au>
2021-05-30 18:13:04 +0800

    heroku

--- front/src/commons/axios.js
+++ front/src/commons/axios.js
@@ -2,7 +2,7 @@ import _axios from 'axios';
 // base url
 const axios = (baseUrl) => {
     const instance = _axios.create({
-        baseURL:"https://pacific-spire-87195.herokuapp.com/" || 'http://localhost:3000'
+        baseURL:"https://rameneat.herokuapp.com/" || 'http://localhost:3000'
     })
     return instance;
 }
